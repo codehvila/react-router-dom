ee9efaf7bf33c85dcb27da6f3c9fe1bdcf240eb2
Hugo Vila <code.hvila@gmail.com>
2022-10-05 14:01:55 +0200

    Configure some routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Home from "./pages/Home";
+import Posts from "./pages/Posts";
 
 function App() {
   return (
     <div className="App">
-      <nav>
-        <h1>My Blog</h1>
-        <a href="/">Home</a>
-        <a href="/posts">Posts</a>
-        <a href="/about">About Our</a>
-        <a href="/contact">Contact</a>
-      </nav>
+      <BrowserRouter>
+        <nav>
+          <h1>My Blog</h1>
+          <a href="/">Home</a>
+          <a href="/posts">Posts</a>
+          <a href="/about">About Our</a>
+          <a href="/contact">Contact</a>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/posts" element={<Posts />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
