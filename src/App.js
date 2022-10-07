c0cc6ef2cd7f744c746e592802276a8f52d3ccaf
Hugo Vila <code.hvila@gmail.com>
2022-10-07 19:00:10 +0200

    Replace unnecessary template literals

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ function App() {
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
 
-          <Route path={`/posts/:postId`} element={<Post />} />
+          <Route path="/posts/:postId" element={<Post />} />
 
           <Route path="*" element={<Navigate to="/" />} />
         </Routes>
